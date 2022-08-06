c840ad05efb55c2488ff4da88ff815ccbd8d05a7
theScarlettCoder <egbadonjeffrey@gmail.com>
2022-08-06 06:34:03 +0100

    Completed Site UI

--- components/ArticleMain.js
+++ components/ArticleMain.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Banner from "../image/banner.png";
 
 import { AiFillPlayCircle } from "react-icons/ai";
 import { IoLogoTwitter } from "react-icons/io";
@@ -12,15 +13,23 @@ import Qazi from "../image/qazi.jpg";
 
 const styles = {
   wrapper: "flex items-center justify-center flex-[3] border-l border-r",
-  content: "h-screen p-[2rem]",
+  content: "h-screen w-full p-[2rem]",
   postHeaderContainer:
-    "flex justify-between items-center mt-[2.2rem] mb-[1.2rem]",
+    "flex justify-between gap-[1rem] items-center mt-[2.2rem] mb-[1.2rem]",
   authorContainer: "flex gap-[1rem]",
+  articleMainContainer: "flex flex-col gap-[1rem]",
   authorProfileImageContainer:
     "h-[3rem] w-[3rem] grid center rounded-full overflow-hidden",
   column: "flex-1 flex flex-col justify-center",
   postDetails: "flex gap-[.2rem] text-[#787878]",
   listenButton: "flex items-center gap-[.2rem] text-[#1A8917]",
+  socials: "flex gap-[1rem] text-[#787878] cursor-pointer",
+  space: "w-[.5rem]",
+  bannerContainer: "h-[20rem] w-full grid center overflow-hidden mb-[2rem]",
+  image: "object-cover",
+  title: "font-bold text-3xl",
+  subtitle: "font-mediumSerifItalic text-[1.4rem] text-[#292929]",
+  articleText: "font-mediumSerif text-[1.4rem] text-[#292929]",
 };
 
 const ArticleMain = () => {
@@ -41,11 +50,9 @@ const ArticleMain = () => {
             <div className={styles.column}>
               <div>Egbadon Jeffrey</div>
               <div className={styles.postDetails}>
-                <span>
-                  June 15 | 7 min read{" "}
-                  <span className={styles.listenButton}>
-                    <AiFillPlayCircle /> Listen
-                  </span>{" "}
+                <span>June 15 | 7 min read</span>{" "}
+                <span className={styles.listenButton}>
+                  <AiFillPlayCircle /> Listen
                 </span>
               </div>
             </div>
@@ -55,10 +62,40 @@ const ArticleMain = () => {
             <FaFacebook />
             <GrLinkedin />
             <HiOutlineLink />
-            <div className={styles.space}>
-              <BiBookmarks />
-              <FiMoreHorizontal />
-            </div>
+            <div className={styles.space} />
+            <BiBookmarks />
+            <FiMoreHorizontal />
+          </div>
+        </div>
+        <div className={styles.articleMainContainer}>
+          <div className={styles.bannerContainer}>
+            <Image
+              className={styles.image}
+              src={Banner}
+              width={100}
+              height={100}
+              alt="Banner"
+            />
+          </div>
+          <h1 className={styles.title}>
+            {" "}
+            7 Free Tools that will make you more productive in 2022{" "}
+          </h1>
+          <h4 className={styles.subtitle}>
+            <div>Egbadon Jeffrey, August 5th</div>
+            <div>Brief: Productivity is a skill that can be learned</div>
+          </h4>
+          <div className={styles.articleText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            voluptatibus saepe unde. Beatae odio porro nisi omnis nam voluptate
+            iure, officiis aperiam dolorem atque nulla molestias. Aliquam sint
+            eligendi similique! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Corporis, architecto. Magni alias aperiam nostrum
+            modi neque, soluta eius adipisci eveniet tempore minus inventore
+            repellat aut beatae totam velit aliquam et temporibus tempora porro,
+            doloremque explicabo commodi reprehenderit? Corrupti, a. Odit,
+            doloremque. Voluptas autem eos repellendus. Ducimus incidunt sunt
+            veniam facilis.
           </div>
         </div>
       </div>
--- components/Recommendations.js
+++ components/Recommendations.js
@@ -64,32 +64,36 @@ const Recommendations = () => {
       <div className={styles.recommendationContainer}>
         <div className={styles.title}>More from Medium</div>
         <div className={styles.articlesContainer}>
-          <div className={styles.articleContentWrapper}>
-            <div className={styles.articleContent}>
-              <div className={styles.recommendationAuthorContainer}>
-                <div
-                  className={styles.recommendationAuthorProfileImageContainer}
-                >
-                  <Image src={Qazi} width={100} height={100} alt="Qazi" />
+          {recommendedPosts.map((post) => (
+            <div className={styles.articleContentWrapper} key={post.id}>
+              <div className={styles.articleContent}>
+                <div className={styles.recommendationAuthorContainer}>
+                  <div
+                    className={styles.recommendationAuthorProfileImageContainer}
+                  >
+                    <Image
+                      src={post.author.image}
+                      width={100}
+                      height={100}
+                      alt={post.author.name}
+                    />
+                  </div>
+                  <div className={styles.recommendationAuthorName}>
+                    {post.author.name}
+                  </div>
                 </div>
-                <div className={styles.recommendationAuthorName}>
-                  Egbadon Jeffrey
-                </div>
+                <div className={styles.recommendationTitle}>{post.title}</div>
               </div>
-              <div className={styles.recommendationTitle}>
-                The Ultimate JavaScript Course for Beginners by Clever
-                Programmer
-              </div>
+              <div className={styles.recommendationThumbnailContainer}></div>
+              <Image
+                className={styles.recommendationThumbnail}
+                src={post.image}
+                width={100}
+                height={100}
+                alt="js logo"
+              />
             </div>
-            <div className={styles.recommendationThumbnailContainer}></div>
-            <Image
-              className={styles.recommendationThumbnail}
-              src={JSLogo}
-              width={100}
-              height={100}
-              alt="js logo"
-            />
-          </div>
+          ))}
         </div>
       </div>
     </div>
@@ -97,3 +101,33 @@ const Recommendations = () => {
 };
 
 export default Recommendations;
+
+const recommendedPosts = [
+  {
+    id: 1,
+    title: "What can you do with Replit ?",
+    image: ReplitLogo,
+    author: {
+      name: "Jeffrey",
+      image: CPLogo,
+    },
+  },
+  {
+    id: 2,
+    title: "How to learn JavaScript Course for Beginners by Qazi.",
+    image: TutorialImg,
+    author: {
+      name: "Qazi",
+      image: Qazi,
+    },
+  },
+  {
+    id: 3,
+    title: "How to Become a Developer in 2022.",
+    image: JSLogo,
+    author: {
+      name: "Clever Programmer",
+      image: JSLogo,
+    },
+  },
+];
